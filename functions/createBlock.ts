52b4d5da98baaa920389a333d86a38e694d1d83e
kohta  <kohtakochi@kawauchimitsumotonoMacBook-Pro.local>
2022-09-24 10:36:36 +0900

    ブロックチェーンの基本構造　完成

--- functions/createBlock.ts
+++ functions/createBlock.ts
@@ -9,5 +9,25 @@ export async function createBlock(
     tx: Tx,
     validator: Validator,
 ): Promise<Block> {
-
+    //現在時刻
+    const time = new Date().toISOString();
+    //インデックス
+    const index = prevBlock.index + 1;
+    //ハッシュ
+    const hash = await calcBlockHash(
+        index,
+        time,
+        prevBlock.hash,
+        tx,
+        validator,
+    );
+    //新しいブロックオブジェクト
+    const  newBlock: Block = {
+        index: index,
+        time: time,
+        prev_hash: prevBlock.hash,
+        hash: hash,
+        tx: tx,
+        validator: validator,
+    }
 };
